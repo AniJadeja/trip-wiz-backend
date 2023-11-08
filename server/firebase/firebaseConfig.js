cd06864ca746fac91caddba5c7e7476f66b666d8
Anirudhdhsinh Jadeja <anirudhdhsinhj@gmail.com>
2023-11-08 00:55:02 -0500

    changes serviceAccount file path

--- server/firebase/firebaseConfig.js
+++ server/firebase/firebaseConfig.js
@@ -1,6 +1,6 @@
 
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = require('../../serviceAccountKey.json');
 
 const config = admin.initializeApp({ credential: admin.credential.cert(serviceAccount), databaseURL: 'https://tripwiz-r-default-rtdb.firebaseio.com/' });
 const realtimeDB = admin.database();
