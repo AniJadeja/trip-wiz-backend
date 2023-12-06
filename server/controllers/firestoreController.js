b279146ff0834a21b0039f505f3da19991fbbe10
Anant Jain <jainanant1108@gmail.com>
2023-12-05 23:08:34 -0500

    updated the backend code for image

--- server/controllers/firestoreController.js
+++ server/controllers/firestoreController.js
@@ -1,64 +1,73 @@
-const { saveUserItinerary } = require('../firebase/manageFirestoreDatabase.js');
-const { getUserItineraries } = require('../firebase/manageFirestoreDatabase.js');
-
+const { saveUserItinerary } = require("../firebase/manageFirestoreDatabase.js");
+const {
+  getUserItineraries,
+} = require("../firebase/manageFirestoreDatabase.js");
 
 // method to accept the user id and then
 // pass it to saveUserItinerary method
 
 exports.saveUserItineraryInCollection = (uid, itinerary, res) => {
-
-  console.log(`firestoreController => uid > ${uid}`)
-  saveUserItinerary(uid, itinerary).then(() => {
-
-    res.status(200).json({ message: 'User itinerary saved successfully' });
-  })
+  console.log(`firestoreController => uid > ${uid}`);
+  saveUserItinerary(uid, itinerary)
+    .then(() => {
+      res.status(200).json({ message: "User itinerary saved successfully" });
+    })
     .catch((error) => {
       // Handle any errors that occur during user creation
-      res.status(400).json({ message: 'User itinerary save failed : error saving user itinerary > ', error: error.message });
+      res.status(400).json({
+        message: "User itinerary save failed : error saving user itinerary > ",
+        error: error.message,
+      });
     });
-}
+};
 
 exports.getUserItinerariesInCollection = (req, res) => {
-
   console.log(`firestoreController => called getUserItinerariesInCollection`);
   const { uid } = req.body;
 
   return new Promise((resolve, reject) => {
     console.log(`firestoreController => calling getUserItineraries`);
-    getUserItineraries(uid).then((itineraries) => {
+    getUserItineraries(uid)
+      .then((itineraries) => {
+        // create an array of the itineraries only without id
 
+        const response = {
+          itineraries,
+        };
 
-      // create an array of the itineraries only without id
+        const itinerariesArray = [];
 
+        // Iterate through each itinerary object
+        for (const itineraryId in response.itineraries) {
+          if (response.itineraries.hasOwnProperty(itineraryId)) {
+            // Get the itinerary object
+            const itinerary = response.itineraries[itineraryId];
 
-      const response = {
-       itineraries
-      };
-
-      const itinerariesArray = [];
-
-      // Iterate through each itinerary object
-      for (const itineraryId in response.itineraries) {
-        if (response.itineraries.hasOwnProperty(itineraryId)) {
-          // Get the itinerary object
-          const itinerary = response.itineraries[itineraryId];
-
-          // Push the itinerary object with its id into the new array
-          itinerariesArray.push({
-            trip_details: itinerary.trip_details,
-            id: itineraryId,
-          });
+            // Push the itinerary object with its id into the new array
+            itinerariesArray.push({
+              trip_details: itinerary.trip_details,
+              id: itineraryId,
+            });
+          }
         }
-      }
 
-
-
-
-      resolve(itineraries);
-      res.status(200).json({ message: 'User itineraries retrieved successfully.', itineraries: itinerariesArray });
-    }).catch((error) => {
-      reject(error);
-      res.status(400).json({ message: 'User itineraries retrieval failed : error retrieving user itineraries > ', error: error.message });
-    });
+        resolve(itineraries);
+        res
+          .status(200)
+          .json({
+            message: "User itineraries retrieved successfully.",
+            itineraries: itinerariesArray,
+          });
+      })
+      .catch((error) => {
+        reject(error);
+        res
+          .status(400)
+          .json({
+            message:
+              "User itineraries retrieval failed : error retrieving user itineraries > ",
+            error: error.message,
+          });
+      });
   });
-}
+};
--- server/controllers/itineraryController.js
+++ server/controllers/itineraryController.js
@@ -1,70 +1,102 @@
 // This module is a controller for itinerary generation.
 
-const ItineraryModel = require('../models/itineraryModel');
-const ItineraryCall = require('../openAI/itinerary.js');
-const {getAndUpdateUserItinerary} = require('../firebase/manageRealtimeDatabase.js');
-const {validateAndConvertToJSON} = require('../utils/jsonValidator.js');
-
+const ItineraryModel = require("../models/itineraryModel");
+const ItineraryCall = require("../openAI/itinerary.js");
+const {
+  getAndUpdateUserItinerary,
+} = require("../firebase/manageRealtimeDatabase.js");
+const { validateAndConvertToJSON } = require("../utils/jsonValidator.js");
+const { getPhotoURL } = require("./placesAPI.js");
 
 exports.generateItinerary = async (req, res) => {
+  console.log(
+    `itineraryController => Itinerary Model: ${JSON.stringify(req.body)}`
+  );
+  const data = req.body;
+  console.log(`itineraryController => data.uid : ${data.uid}`);
+  const itineraryModel = new ItineraryModel({
+    uid: data.uid,
+    destination: data.destination,
+    startDate: data.startDate,
+    endDate: data.endDate,
+    placesToVisit: data.placesToVisit,
+    tripType: data.tripType,
+    numberOfDays: data.numberOfDays,
+  });
 
-    console.log(`itineraryController => Itinerary Model: ${JSON.stringify(req.body)}`)
-    const data = req.body;
-    console.log(`itineraryController => data.uid : ${data.uid}`)
-    const itineraryModel = new ItineraryModel({
-        uid: data.uid,
-        destination: data.destination,
-        startDate: data.startDate,
-        endDate: data.endDate,
-        placesToVisit: data.placesToVisit,
-        tripType: data.tripType,
-        numberOfDays: data.numberOfDays,
-    });
+  const itinerary = new ItineraryCall(itineraryModel);
+  console.log(
+    "itineraryController => before generateItinerary uid: " + itineraryModel.uid
+  );
+  itinerary
+    .generateItinerary()
+    .then(async (itinerary) => {
+      let itineraryJSON = validateAndConvertToJSON(itinerary);
 
-    const itinerary = new ItineraryCall(itineraryModel);
-    console.log('itineraryController => before generateItinerary uid: ' + itineraryModel.uid);
-    itinerary.generateItinerary().then((itinerary) => {
-        let itineraryJSON = validateAndConvertToJSON(itinerary);
-        const uid = itineraryModel.uid;
-        console.log(`itineraryController => did call generateItinerary() sucessfully`);
-        console.log(`itineraryController => updating itinerary in the database`);
-        
-        getAndUpdateUserItinerary(uid, itineraryJSON).then(() => {
-          console.log('itineraryController => itinerary updated in the database');
-          res.status(200).json({ message: 'Itinerary Generated Sucessfully', itinerary: `${JSON.stringify(itineraryJSON)}` });
-        }).catch((err) => { 
-          console.log(`itineraryController => Error updating the itinerary in the database: ${err}`);
-          res.status(500).json({ message: 'Error updating the itinerary in the database', suggestedAction: 'Please try again later or contact the administrator.' });
-        });
+      itineraryJSON.trip_details.itinerary = await Promise.all(
+        itineraryJSON.trip_details.itinerary.map(async (day) => ({
+          ...day,
+          places: await Promise.all(
+            day.places.map(async (place) => ({
+              ...place,
+              photoUrl: await getPhotoURL(place.name).catch(() => ""),
+            }))
+          ),
+        }))
+      );
+      const uid = itineraryModel.uid;
+      console.log(
+        `itineraryController => did call generateItinerary() sucessfully`
+      );
+      console.log(`itineraryController => updating itinerary in the database`);
 
-   
-       
-    })
+      getAndUpdateUserItinerary(uid, itineraryJSON)
+        .then(() => {
+          console.log(
+            "itineraryController => itinerary updated in the database"
+          );
+          res.status(200).json({
+            message: "Itinerary Generated Sucessfully",
+            itinerary: `${JSON.stringify(itineraryJSON)}`,
+          });
+        })
         .catch((err) => {
-            console.log(`itineraryController => Error generating the itinerary: ${err}`)
-            res.status(500).json({ message: 'Error generating the itinerary', suggestedAction: 'Please try again later or contact the administrator.' });
-        }
-        );
-}
-
-
-function getLatestItinerary (uid) {
-  return new Promise((resolve, reject) => {
-    realtimeDB.ref('itineraries/' + uid).once('value').then((snapshot) => {
-      const itinerary = snapshot.val();
-      resolve(itinerary);
-    }).catch((err) => {
-      reject(err);
+          console.log(
+            `itineraryController => Error updating the itinerary in the database: ${err}`
+          );
+          res.status(500).json({
+            message: "Error updating the itinerary in the database",
+            suggestedAction:
+              "Please try again later or contact the administrator.",
+          });
+        });
+    })
+    .catch((err) => {
+      console.log(
+        `itineraryController => Error generating the itinerary: ${err}`
+      );
+      res.status(500).json({
+        message: "Error generating the itinerary",
+        suggestedAction: "Please try again later or contact the administrator.",
+      });
     });
+};
+
+function getLatestItinerary(uid) {
+  return new Promise((resolve, reject) => {
+    realtimeDB
+      .ref("itineraries/" + uid)
+      .once("value")
+      .then((snapshot) => {
+        const itinerary = snapshot.val();
+        resolve(itinerary);
+      })
+      .catch((err) => {
+        reject(err);
+      });
   });
 }
 
-exports.getItinerary = async (req, res) => {
+exports.getItinerary = async (req, res) => {};
 
-
-}
-
-
-exports.saveItinerary = async (req, res) => {
-  
-}
+exports.saveItinerary = async (req, res) => {};
